e10a740a125267ef2902bb060288475f38d681d4
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-03-08 11:42:58 -0500

    add created by, fix routes, fix database error

--- controllers/home.js
+++ controllers/home.js
@@ -7,6 +7,7 @@ module.exports = {
     getIndex: async (req,res)=>{
         console.log(req.user)
         const recipes = await Recipe.find().sort({ createdAt: 1 }).lean();
+        console.log(recipes)
         res.render('index.ejs', {recipes: recipes})
     }
 }
